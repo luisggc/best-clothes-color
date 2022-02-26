0a7c3cea19d7c456cfd79f42226565e022068c55
Luis <luiscoimbraeng@outlook.com>
2022-02-26 18:08:54 -0300

    First Version Completed

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <div>
+      <h3>Best Clothes Color</h3>
+      <p>Just click the clothes to select a new color</p>
+      </div>
+      <div>
+        <i style={{ fontSize: 24 }} className="fa">
+          &#xf09b;
+        </i>
+      </div>
+    </header>
+  );
+}
