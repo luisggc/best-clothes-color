041cfdfe2ce005fb59fe2253345c73498d368460
Luis <luiscoimbraeng@outlook.com>
2022-02-27 11:43:40 -0300

    ReadMe

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+# Best Clothes Color
+
+[Click here](https://best-clothes-color.netlify.app/) to see the demo.
+
+- SVG + Image Manipulation
+- ColorPicker and position absolute
+- useState
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,15 @@ export default function Header() {
   return (
     <header>
       <div>
-      <h3>Best Clothes Color</h3>
-      <p>Just click the clothes to select a new color</p>
+        <h3>Best Clothes Color</h3>
+        <p>Just click the clothes to select a new color</p>
       </div>
       <div>
-        <i style={{ fontSize: 24 }} className="fa">
-          &#xf09b;
-        </i>
+        <a href="https://github.com/luisggc/best-clothes-color" style={{ color: "white" }} target="_blank">
+          <i style={{ fontSize: 24 }} className="fa">
+            &#xf09b;
+          </i>
+        </a>
       </div>
     </header>
   );
